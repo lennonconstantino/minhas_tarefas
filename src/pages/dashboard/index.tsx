920755b07b6a088f00cbc8c164ea33821d4894c8
lennonconstantino <lennonconstantino@gmail.com>
2025-06-25 18:28:08 -0300

    criando dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
+
 import { getSession } from "next-auth/react";
+import { Textarea } from "../../components/textarea";
 
 export default function Dashboard() {
   return (
@@ -10,7 +12,25 @@ export default function Dashboard() {
         <title>Meu painel de tarefas</title>
       </Head>
 
-      <h1>Pagina Painel</h1>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+            <form>
+              <Textarea placeholder="Digite qual sua tarefa..." />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa publica?</label>
+              </div>
+
+              <button className={styles.button} type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
