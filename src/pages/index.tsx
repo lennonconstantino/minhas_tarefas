ed8dfcaa239d6e9c8fa75815df0de779eef6534b
lennonconstantino <lennonconstantino@gmail.com>
2025-06-25 16:26:06 -0300

    criando header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,20 +7,30 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Tarefas+ | Organize suas tarefas de forma facil</title>
+        <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
       </Head>
+
       <main className={styles.main}>
         <div className={styles.logoContent}>
           <Image
             className={styles.hero}
-            alt="Logo tarefas+"
+            alt="Logo Tarefas+"
             src={heroImg}
             priority
           />
-          <h1 className={styles.title}>
-            Sistema feito para você organizar seus <br/> 
-            estudos e tarefas
-          </h1>
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br />
+          seus estudos e terefas
+        </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
         </div>
       </main>
     </div>
